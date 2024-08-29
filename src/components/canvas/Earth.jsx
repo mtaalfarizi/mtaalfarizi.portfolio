fdba0b3417c3ee5addbaabef45327d1b19eac98c
Muhammad Thoriq Alfarizi <mtaalfarizi@gmail.com>
2024-08-30 04:15:36 +0700

    first

---
+++ src/components/canvas/Earth.jsx
@@ -0,0 +1,65 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
+
+const Earth = () => {
+  const earth = useGLTF("./planet/scene.gltf");
+
+  return (
+    <primitive object={earth.scene} scale={3} position-y={-1} rotation-y={1} />
+  );
+};
+
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      shadows
+      frameloop='demand'
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-5, 3, 6],
+      }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+
+        {/* Ambient Light */}
+        <ambientLight intensity={0.3} />
+
+        {/* Directional Light */}
+        <directionalLight 
+          position={[-5, 5, 5]} 
+          intensity={1} 
+          castShadow 
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+
+        {/* Point Light */}
+        <pointLight position={[5, 5, 5]} intensity={0.5} />
+
+        <Earth />
+
+        <Preload all />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+export default EarthCanvas;
---
+++ src/constants/index.js
@@ -0,0 +1,173 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Mechanical Engineering Student",
+    icon: mobile,
+  },
+  {
+    title: "Data Manager",
+    icon: mobile,
+  },
+  {
+    title: "Web Developer",
+    icon: mobile,
+  },
+  {
+    title: "Robotics",
+    icon: mobile,
+  },
+];
+
+const technologies = [
+
+];
+
+const experiences = [
+  {
+    title: "Data Manager",
+    company_name: "Universitas Indonesia",
+    icon: starbucks,
+    iconBg: "#383E56",
+    date: "March 2020 - April 2021",
+    points: [
+      "Oversaw all aspects of the Tracer Study project, including planning, execution, and evaluation, impacting over 5,000 graduates annually.",
+      "Developed survey instruments and methodologies, resulting in a 30% increase in response rates from alumni through online surveys, interviews, and other relevant methods.",
+      "Analyzed collected data from 4,500+ alumni to identify trends, patterns, and insights, contributing to a 15% improvement in program effectiveness and alumni career outcomes.",
+    ],
+  },
+];
+
+const testimonials = [ {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "First Personal Portfolio",
+    description:
+      "Using only HTML, CSS, and JavaScript, I created a responsive portfolio website that showcases my skills and projects.",
+    tags: [
+      {
+        name: "html",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "css",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "javascript",
+        color: "yellow-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/mtaalfarizi/personal-portfolio-1",
+  },
+  {
+    name: "Robot Arm",
+    description:
+      "It is created using Arduino UNO, couple of joysticks, breadboards, and the model to the arm itself using acrylic. My take on this project is i want to create something that CAN be used by myself, that actually really useful.",
+    tags: [
+      {
+        name: "physical",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "microcontroller",
+        color: "green-text-gradient",
+      },
+      {
+        name: "robotics",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "3D Portfolio Website",
+    description:
+      "The upgrade of my previous take on UI/UX. It is created using three.js, tailwind, and other JavaScript frameworks. Interactivity is a small part of this design, yet I still want to learn more about it.",
+    tags: [
+      {
+        name: "threejs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "javascript",
+        color: "yellow-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
